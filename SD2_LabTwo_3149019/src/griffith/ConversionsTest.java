4c2d47e9f1d75bcd9bed1d2be8dff269fabd5b98
Angelo Jose <angelo-mmacedo@hotmail.com>
2026-02-25 17:05:20 +0000

    I Added JUnit 5 Library to the Folder and Ran the First Test

--- SD2_LabTwo_3149019/src/griffith/ConversionsTest.java
+++ SD2_LabTwo_3149019/src/griffith/ConversionsTest.java
@@ -1,6 +1,43 @@
 package griffith;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+//Name: Angelo Jose Medeiros de Macedo Filho | Student N: 3149019
+
 public class ConversionsTest {
-	public static void main(String[] args) {
+	
+	Conversions c = new Conversions();
+	
+	// Test for euroToDollar method
+	// failing test to commit
+	@Test
+	public void testEuroToDollar() {
+		double euro = 110.0;
+		assertEquals(110.0, c.euroToDollar(euro), 0.001);
+		
+		double negativeEuro	 = -50.0;
+		assertEquals(-55.0, c.euroToDollar(negativeEuro), 0.001);
+		
+		double zeroEuro = 0.0;
+		assertEquals(0.0, c.euroToDollar(zeroEuro), 0.001);
+		
+	}
+	
+	
+	public void testDollarToEuro() {
+		
+	}
+	
+	public void testStringToInteger() {
+	
+	}
+	
+	public void testIntegerToString() {
+	
+	}
+	
+	public void testSwitchCase() {
+	
 	}
 }
